9a0c7e4776d6da6b1b06703fef4d58576b25426b
SalvatoreMilone <salvatore.milone@outlook.it>
2024-11-12 14:22:58 +0100

    animation jumbo

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,79 @@ const navbarLogo = document.getElementById("navbarLogo");
 const navbarBtnClose = document.querySelector(".btn-close");
 const navbarXL = document.querySelector(".navbarXL");
 const mobileNav = document.getElementById("mobileNav");
+
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 150, density: { enable: true, value_area: 800 } },
+    color: { value: "#ffffff" },
+    shape: {
+      type: "circle",
+      stroke: { width: 0, color: "#000000" },
+      polygon: { nb_sides: 5 },
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 6,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: { enable: false, rotateX: 600, rotateY: 1200 },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: true, mode: "bubble" },
+      onclick: { enable: true, mode: "repulse" },
+      resize: true,
+    },
+    modes: {
+      grab: { distance: 400, line_linked: { opacity: 1 } },
+      bubble: { distance: 400, size: 10, duration: 2, opacity: 4, speed: 3 },
+      repulse: { distance: 200, duration: 0.4 },
+      push: { particles_nb: 4 },
+      remove: { particles_nb: 2 },
+    },
+  },
+  retina_detect: true,
+});
+var count_particles, stats, update;
+stats = new Stats();
+stats.setMode(0);
+stats.domElement.style.position = "absolute";
+stats.domElement.style.left = "0px";
+stats.domElement.style.top = "0px";
+document.body.appendChild(stats.domElement);
+count_particles = document.querySelector(".js-count-particles");
+update = function () {
+  stats.begin();
+  stats.end();
+  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+  }
+  requestAnimationFrame(update);
+};
+requestAnimationFrame(update);
+
 /* 
 -Event listener to the document for the "scroll" event. When the user
 scrolls, logo and navbar will be adapted.
@@ -85,7 +158,7 @@ const data2 = {
       data: [40, 60],
       backgroundColor: ["#4da0f2", "rgba(0,0,0,.1)"],
       hoverOffset: 4,
-      rotation: 90
+      rotation: 90,
     },
   ],
 };
@@ -97,7 +170,7 @@ const data3 = {
       data: [40, 60],
       backgroundColor: ["#3ed29f", "rgba(0,0,0,.1)"],
       hoverOffset: 4,
-      rotation: 180
+      rotation: 180,
     },
   ],
 };
